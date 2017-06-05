a6c17377223991ec141889e07f7e822f00339855
Prescillia San <presci_9@hotmail.fr>
2017-06-05 17:00:37 +0200

    [FRONT] Delete fetch_displayMovies and loadMovies method in movie-api

--- frontend/src/api/movie-api.js
+++ frontend/src/api/movie-api.js
@@ -5,24 +5,6 @@ import 'isomorphic-fetch'
 import store from '../store'
 import * as movie_actions from '../actions/movie-actions'
 
-export function fetch_DisplayMovies() {
-    console.log("fetch display movies");
-    return (dispatch) => {
-        return loadMovies().then((json) => {
-            console.log("json: ", json)
-            dispatch(movie_actions.displayMovies(json))
-        })
-    }
-}
-
-function loadMovies() {
-    console.log("Load movies from server");
-    return fetch("http://localhost:8080/movies")
-        .then(response => {
-            return response.json()
-        })
-}
-
 export function getAllMovies() {
     return fetch('http://localhost:8080/movie')
         .then(response => response.json())
