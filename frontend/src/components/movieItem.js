abf0724f93bf53c5d68383c0c7a92d7a495078a6
Prescillia San <presci_9@hotmail.fr>
2017-06-06 13:58:54 +0200

    [FRONTEND] Change weird name class to the same as those in movies.html

--- frontend/src/components/movieItem.js
+++ frontend/src/components/movieItem.js
@@ -7,19 +7,27 @@ import templatemo from '../css/templatemo-style.css'
 import bootstrap from '../css/bootstrap.min.css'
 
 const MovieItem = ({ data }) => (
-    <div className="caca">
-        <div className="toto">
-            <p className="pipi">{data.name}</p>
-            <p className="pipi">{data.realisator}</p>
+    <div className="tm-movies-box-1-info">
+        <div className="tm-movies-box-1-info-left">
+            <p className="text-uppercase margin-bottom-20 title">{data.name}</p>
+            <p className="realisator">{data.realisator}</p>
             <p className="release-date">{data.release}</p>
             <p className="duration">{data.time}</p>
             <p className="type">{data.genre}</p>
-            <img src="../img/rating.png" alt="image"/>
+            <img src="../img/rating.png" alt="image" className="margin-bottom-5"/>
         </div>
-        <div className="ok">
+        <div className="tm-movies-box-1-info-right">
             <p className="description">
                 {data.description}
             </p>
+        </div>
+        <div className="tm-movies-box-1-link">
+            <div className="tm-movies-box-1-link-left available">
+                Disponible
+            </div>
+            <a href="schedules.html" className="tm-movies-box-1-link-right schedule">
+                Séances
+            </a>
         </div>
     </div>
 );
