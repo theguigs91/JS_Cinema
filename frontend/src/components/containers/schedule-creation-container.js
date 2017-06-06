460a195131c9345e10ad2bb20a21a80421418e65
Kelly Luu <luu_k@epita.fr>
2017-06-06 15:25:13 +0200

    [FRONT] Schedule Creation - Set movies, rooms list, and compute schedules according of the selected movie in select input

--- frontend/src/components/containers/schedule-creation-container.js
+++ frontend/src/components/containers/schedule-creation-container.js
@@ -1,58 +1,124 @@
 import React from 'react';
-import * as movieApi from '../../api/movie-api';
 import * as scheduleApi from '../../api/schedule-api';
+import * as movieApi from '../../api/movie-api';
+import * as roomApi from '../../api/room-api';
 import ScheduleCreation from '../views/schedule-creation-form';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import { HHMMSSToSeconds, HHMMToSeconds, secondsToHHMM } from '../../helpers/time-helper';
+import { dateToYYYYMMDD } from '../../helpers/date-helper';
 
-function validate(movie) {
+const validate = function(schedule) {
   const errors = {};
-  if (!movie.name || movie.name.trim === '')
-    errors.id = 'Veuillez choisir un film';
-  if (!movie.director || movie.director.trim === '')
-    errors.realisator = 'Veuillez entrer le nom du réalisateur.';
-  if (!movie.date || movie.date.trim === '' )
-    errors.date = 'Veuillez choisir une date de sortie.';
-  if (!movie.duration || movie.duration.trim === '')
-    errors.duration = 'Veuillez entrer une durée.';
-  if (!movie.type || movie.type.trim === '')
-    errors.type = 'Veuillez choisir un ou plusieurs genres.';
-  if (!movie.description || movie.description.trim === '')
-    errors.description = 'Veuillez entrer une description.';
+
+  if (!schedule.movie)
+    errors.movie = 'Veuillez choisir un film';
+  if (!schedule.room || schedule.room.trim === '')
+    errors.room = 'Veuillez choisir une salle.';
+  if (!schedule.schedules || _.isEmpty(schedule.schedules))
+    errors.schedules = 'Veuillez sélectionner au moins une horaire.';
+  if (!schedule.date_start || schedule.date_start.trim === '')
+    errors.date_start = 'Veuillez choisir une date de début.';
+  if (!schedule.date_end || schedule.date_end.trim === '')
+    errors.date_end = 'Veuillez entrer une date de fin.';
 
   return errors;
-}
+};
 
-const MovieCreationContainer = React.createClass({
+const ScheduleCreationContainer = React.createClass({
 
-  componentDidMount: function() {
-    // TODO: Not implemented yet.
-    // let movie = movieApi.getMovie(n)
+  componentWillMount: function() {
+    movieApi.getMovies();
+    roomApi.getRooms();
   },
 
-  addMovie: function(event) {
+  getComputedSchedules: function(movie) {
+    console.log('getComputedSchedules', movie);
+    console.log(movie["time"]);
+    let min = (10 - movie.time.split(':')[1] % 10) * 60; // Minutes to add for celling duration (01:34 -> 01:40)
+    let duration = HHMMSSToSeconds(movie.time);
+    let laps = duration + min + 10 * 60; // 10 minutes between each seance, to clean up the room.
+    let schedules = [];
+    // Let's assume the cinema is open from 9:30 to 23:00
+    let start = HHMMToSeconds('09:30');
+    let end = HHMMToSeconds('23:00');
+
+    for (let s = start; s + laps < end; s += laps)
+      schedules.push(secondsToHHMM(s));
+
+    console.log("Computed schedules: ", schedules);
+    return schedules;
+  },
+
+  /**
+   * Update schedules according to the selected movie.
+   */
+  handleMovieChange: function(event) {
+    this.setState({movie: event.target.value});
+  },
+
+  addSeance: function(event) {
     event.preventDefault();
 
-    let movie = this.refs.child.getMovie();
-    let errors = validate(movie);
-    if (errors === {})
-      movieApi.addMovie(movie);
+    let formInfos = this.refs.child.getFormInfos();
+    console.log('formInfos: ', formInfos);
+    let errors = validate(formInfos);
+    if (_.isEmpty(errors)) {
+
+      let start = new Date(formInfos.date_start);
+      let end = new Date(formInfos.date_end);
+
+      // Iterate over days and schedule of each, and add seance.
+      while(start < end) {
+        start.setDate(start.getDate() + 1);
+
+        formInfos.schedules.map(schedule => {
+
+          let seance = {
+            movie_id: formInfos.movie.id,
+            room_id: formInfos.room.id,
+            places_available: formInfos.room.places_max,
+            date: dateToYYYYMMDD(start, '-'),
+            time: schedule
+          };
+          console.log(seance);
+          //scheduleApi.addSchedule(seance);
+        });
+      }
+    }
     else {
       console.log(errors);
     }
   },
 
   render: function() {
-    return (
-      <MovieCreation addMovie={this.addMovie} ref="child"/>
-    );
-  }
+    console.log('ScheduleCreationContainer.render');
+    let schedules = [];
 
+    // Compute schedules of selected movie
+    if (this.props.movies.length !== 0) {
+      let movie = (this.refs.child.getMovie()) ? this.refs.child.getMovie() : this.props.movies[0];
+      schedules = this.getComputedSchedules(movie);
+    }
+    return (
+      <ScheduleCreation
+        addSchedule={this.addSeance}
+        movies={this.props.movies}
+        rooms={this.props.rooms}
+        schedules={schedules}
+        handleMovieChange={this.handleMovieChange}
+        ref="child"
+      />
+    )
+  }
 });
 
-const mapStateToProps = function(store) {
+const mapStateToProps = store => {
   return {
-    movie: store.movieState.movie
+    schedule: store.scheduleState.schedule,
+    movies: store.movieState.movies,
+    rooms: store.roomState.rooms
   };
 };
 
-export default connect(mapStateToProps)(MovieCreationContainer);
+export default connect(mapStateToProps)(ScheduleCreationContainer);
