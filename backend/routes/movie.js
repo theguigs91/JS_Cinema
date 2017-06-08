4092692bed6b427f01e6cf3ae055f3c9448282cf
Prescillia San <presci_9@hotmail.fr>
2017-06-08 11:45:43 +0200

    [BACKEND] Change the response for getting a movie

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -89,7 +89,10 @@ router.get('/name/:name', function(req, res, next) {
 
     connection.query(queryString, param, function(err, rows, fields) {
         if (!err) {
-            res.status(200).send(JSON.stringify({rows: rows}));
+          res.status(200);
+          res.json(rows);
+          res.end();
+           // res.status(200).send(JSON.stringify({rows: rows}));
         }
         else {
             res.status(400).send(JSON.stringify({message: err}));
