eedf3ccc0ca126ba226872a7c002a4e125da80e4
Prescillia San <presci_9@hotmail.fr>
2017-06-08 11:50:12 +0200

    [FRONTEND] Can search any film in result.html

--- frontend/src/reducers/movie-reducer.js
+++ frontend/src/reducers/movie-reducer.js
@@ -7,26 +7,29 @@ const initialState = {
 
 const movieReducer = function (state = initialState, action) {
 
-    switch (action.type) {
-        case 'ADD_MOVIE':
-            return [...state, {movies: action.payload}];
-        case 'DELETE_MOVIE':
-            return state.filter(movie => movie.id !== action.id);
-        case types.GET_ALL_MOVIES:
-            return {...state, movies: action.payload};
-        case types.GET_ALL_MOVIES_FROM_DATE:
-            return {...state, movies: action.payload};
-        case types.GET_MOVIES_SUCCESS:
-            return {...state, movies: action.payload};
-        case types.GET_MOVIE_BY_ID:
-            return state;
-        case types.GET_MOVIE_BY_NAME:
-            return state;
-        case types.UPDATE_MOVIE_BY_ID:
-            return state;
-        default:
-            return state;
+  console.log("Action type: ", action.type);
+  console.log("Action payload: ", action.payload);
+
+  switch (action.type) {
+      case 'ADD_MOVIE':
+          return [...state, {movies: action.payload}];
+      case 'DELETE_MOVIE':
+          return state.filter(movie => movie.id !== action.id);
+      case types.GET_ALL_MOVIES:
+          return {...state, movies: action.payload};
+      case types.GET_ALL_MOVIES_FROM_DATE:
+          return {...state, movies: action.payload};
+      case types.GET_MOVIES_SUCCESS:
+          return {...state, movies: action.payload};
+      case types.GET_MOVIE_BY_ID:
+          return state;
+      case types.GET_MOVIE_BY_NAME:
+          return state;
+      case types.UPDATE_MOVIE_BY_ID:
+          return state;
+      default:
+          return state;
     }
-}
+};
 
 export default movieReducer
