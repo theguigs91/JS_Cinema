9549796edf299393c24cc9e5bf65bf66d355b7d9
Kelly Luu <luu_k@epita.fr>
2017-06-08 11:30:21 +0200

    [FRONTEND] Reducer - fix delete cases

--- frontend/src/reducers/reservation-reducer.js
+++ frontend/src/reducers/reservation-reducer.js
@@ -7,11 +7,14 @@ const initialReservationState = {
 
 const ReservationReducer = function(state: initialReservationState, action) {
 
+  console.log("Action type: ", action.type);
+  console.log("Action payload: ", action.payload);
+
   switch (action.type) {
     case types.ADD_RESERVATION_SUCCESS:
       return { ...state, reservations: [...state.reservations, action.reservation] };
     case types.DELETE_RESERVATION_SUCCESS:
-      return state.filter(p => p.id !== action.id);
+      return {...state, reservations: state.reservations.filter(p => p.id !== action.id)};
     case types.DISPLAY_RESERVATIONS:
       return {...state, reservations: action.payload};
     case types.GET_RESERVATIONS_SUCCESS:
