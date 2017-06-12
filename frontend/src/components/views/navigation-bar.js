769c28d25184479f38948593fb323dc8dc0ab801
Kelly Luu <luu_k@epita.fr>
2017-06-12 16:57:28 +0200

    Navigation bar - Fix Link bug by using react-router 2.0.0

--- frontend/src/components/views/navigation-bar.js
+++ frontend/src/components/views/navigation-bar.js
@@ -2,13 +2,12 @@
  * Created by presci on 12/06/17.
  */
 import React, { PropTypes } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 class NavigationBar extends React.Component {
 
   render() {
 
-    console.log("Render NAVIGATION BAR")
     return (
       <div>
         <ul>
@@ -18,7 +17,7 @@ class NavigationBar extends React.Component {
           <li><Link to="/">A propos</Link></li>
         </ul>
       </div>
-        )
+    );
   }
 }
 
