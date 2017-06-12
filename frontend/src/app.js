4454e4948d77180c8a07ff17aa7b690566bf01fb
Guillaume Audinet <audine_g@epita.fr>
2017-06-12 02:32:44 +0200

    Add import CSS files in app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,6 +5,12 @@ import store from './store';
 // TODO: MUIThemeProvider
 import { App } from './router';
 
+import s1 from '../src/misc/css/templatemo-style.css';
+import s2 from '../src/misc/css/font-awesome.min.css';
+import s3 from '../src/misc/css/flexslider.css';
+import s4 from '../src/misc/css/bootstrap-datetimepicker.min.css';
+import s6 from '../src/misc/css/bootstrap.min.css';
+
 // State will "flow" down from here thanks to the Provider component
 
 render(
