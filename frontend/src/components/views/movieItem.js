4dfaff2fea85ccfcdf8b1369403bcd7fd52f2d1f
Kelly Luu <luu_k@epita.fr>
2017-06-13 01:49:19 +0200

    [FRONTEND] Movies - add missing div tag (row tm-movies-box-1)

--- frontend/src/components/views/movieItem.js
+++ frontend/src/components/views/movieItem.js
@@ -22,19 +22,21 @@ class MovieItem extends React.Component {
     console.log("[MovieItem] Rendering ...", this.props);
 
     return (
-      <div className="tm-movies-box-1-info">
-        <div className="tm-movies-box-1-info-left">
-          <p className="text-uppercase margin-bottom-20 title">{this.props.data.name}</p>
-          <p className="realisator">{this.props.data.realisator}</p>
-          <p className="release-date">{this.props.data.release}</p>
-          <p className="duration">{this.props.data.time}</p>
-          <p className="type">{this.props.data.genre}</p>
-          <img src='../../misc/img/cinema.jpg' alt="image" className="margin-bottom-5"/>
-        </div>
-        <div className="tm-movies-box-1-info-right">
-          <p className="description">
-            {this.props.data.description}
-          </p>
+      <div className="row tm-movies-box-1">
+        <div className="tm-movies-box-1-info">
+          <div className="tm-movies-box-1-info-left">
+            <p className="text-uppercase margin-bottom-20 title">{this.props.data.name}</p>
+            <p className="realisator">{this.props.data.realisator}</p>
+            <p className="release-date">{this.props.data.release}</p>
+            <p className="duration">{this.props.data.time}</p>
+            <p className="type">{this.props.data.genre}</p>
+            <img src='../../misc/img/cinema.jpg' alt="image" className="margin-bottom-5"/>
+          </div>
+          <div className="tm-movies-box-1-info-right">
+            <p className="description">
+              {this.props.data.description}
+            </p>
+          </div>
         </div>
       </div>
     )
