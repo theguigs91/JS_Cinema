a42edea7ae5585ed751483a37a54196c990b6c4f
Kelly Luu <luu_k@epita.fr>
2017-06-12 15:19:59 +0200

    Reservation Creation Container - Remove useless comments

--- frontend/src/components/containers/reservation-creation-container.js
+++ frontend/src/components/containers/reservation-creation-container.js
@@ -13,30 +13,6 @@ class ReservationCreationContainer extends React.Component {
 
   componentWillMount() {
 
-    /*this.movie = {
-      "id": 3,
-      "name": "Problemos",
-      "realisator": "Eric Judor",
-      "time": "01:25:00",
-      "genre": "Comédie",
-      "description": "Jeanne et Victor sont deux jeunes Parisiens de retour de vacances. En chemin, ils font une halte pour saluer leur ami Jean-Paul, sur la prairie où sa communauté a élu résidence.",
-      "date": "2017-10-09T22:00:00.000Z"
-    };
-    this.seance = {
-      "id": 6,
-      "room_id": 2,
-      "movie_id": 3,
-      "places_available": 368,
-      "date": "2017-05-15T22:00:00.000Z",
-      "time": "22:00:00"
-    };
-    this.room = {
-      "id": 2,
-      "numero": 2,
-      "places_max": 368
-    };*/
-
-
     this.seanceId = this.props.match.params.seanceId;
 
     console.log('[ReservationCreationContainer] seanceId = ', this.seanceId);
