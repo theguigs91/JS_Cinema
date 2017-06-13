a76014dfc70a7597604c6d3f643c983ce0ff2c09
Kelly Luu <luu_k@epita.fr>
2017-06-13 14:35:01 +0200

    [FRONTEND] DateHelper - add dateToDDMMYYYY function

--- frontend/src/helpers/date-helper.js
+++ frontend/src/helpers/date-helper.js
@@ -21,10 +21,33 @@ export const dateToYYYYMMDD = function(date, joinStr) {
 };
 
 /**
+ * Convert Date (new Date type) to string format (ddmmyyyy),
+ * each element separated by joinStr.
+ *
+ * @param date: Date
+ * @param joinStr: String used to separate elements ('-' -> dd-mm-yyyy)
+ * @returns {string}
+ */
+export const dateToDDMMYYYY = function(date, joinStr) {
+  let mm = date.getMonth() + 1; // getMonth() is zero-based
+  let dd = date.getDate();
+
+  return [(dd > 9 ? '' : '0') + dd,
+    (mm > 9 ? '' : '0') + mm,
+    date.getFullYear()
+  ].join(joinStr);
+};
+
+
+/**
  * Get yyyy-mm-dd from yyyy-MM-dd'T'HH:mm:ss.SSSZ
  */
 export const getYYYYMMDD = function(dateStr) {
   return dateStr.substring(0, 10);
+};
+
+export const getTodayDateDDMMYYYY = function(joinStr) {
+  return dateToDDMMYYYY(new Date(), joinStr);
 };
 
 let start = new Date();
